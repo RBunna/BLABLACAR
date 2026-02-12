2074cfc0e02bf74eb741f39b8fa247d8dc5bbda3
RBunna <bunnasovethroath@gmail.com>
2026-02-12 23:39:38 +0700

    BLA-003 - Implement Form

--- lib/ui/screens/ride_pref/widgets/ride_pref_form.dart
+++ lib/ui/screens/ride_pref/widgets/ride_pref_form.dart
@@ -1,8 +1,11 @@
+import 'package:blabla/ui/theme/theme.dart';
+import 'package:blabla/ui/widgets/display/bla_divider.dart';
+import 'package:blabla/utils/date_time_util.dart';
 import 'package:flutter/material.dart';
- 
+
 import '../../../../model/ride/locations.dart';
 import '../../../../model/ride_pref/ride_pref.dart';
- 
+
 ///
 /// A Ride Preference From is a view to select:
 ///   - A depcarture location
@@ -28,8 +31,6 @@ class _RidePrefFormState extends State<RidePrefForm> {
   Location? arrival;
   late int requestedSeats;
 
-
-
   // ----------------------------------
   // Initialize the Form attributes
   // ----------------------------------
@@ -37,18 +38,33 @@ class _RidePrefFormState extends State<RidePrefForm> {
   @override
   void initState() {
     super.initState();
-    // TODO 
+    // TODO
+    departureDate = DateTime.now();
+    requestedSeats = 1;
   }
 
   // ----------------------------------
   // Handle events
   // ----------------------------------
- 
 
   // ----------------------------------
   // Compute the widgets rendering
   // ----------------------------------
-  
+  Text _titleEnable(String name) => Text(
+    name,
+    style: BlaTextStyles.button.copyWith(color: BlaColors.textNormal),
+  );
+  Text _titleLight(String name) => Text(
+    name,
+    style: BlaTextStyles.button.copyWith(color: BlaColors.textLight),
+  );
+  Text get departureTitle => departure != null
+      ? _titleEnable(departure!.name)
+      : _titleLight('Leaving from');
+  Text get arrivalTitle =>
+      arrival != null ? _titleEnable(arrival!.name) : _titleLight('Going to');
+  Text get date => _titleEnable(DateTimeUtils.formatDateTime(departureDate));
+  Text get seat => _titleEnable(requestedSeats.toString());
 
   // ----------------------------------
   // Build the widgets
@@ -56,10 +72,38 @@ class _RidePrefFormState extends State<RidePrefForm> {
   @override
   Widget build(BuildContext context) {
     return Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [ 
- 
-        ]);
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Row(
+          children: [
+            Expanded(
+              child: ListTile(
+                leading: Icon(Icons.circle_outlined),
+                title: departureTitle,
+              ),
+            ),
+            if (departure != null || arrival != null) IconButton(onPressed: () {}, icon: Icon(Icons.swap_vert_outlined, color: BlaColors.primary,)),
+          ],
+        ),
+        BlaDivider(),
+        ListTile(leading: Icon(Icons.circle_outlined), title: arrivalTitle),
+        BlaDivider(),
+        ListTile(leading: Icon(Icons.calendar_month_outlined), title: date),
+        BlaDivider(),
+        ListTile(leading: Icon(Icons.person_outline), title: seat),
+        Container(
+          decoration: BoxDecoration(
+            color: BlaColors.primary,
+            borderRadius: BorderRadius.vertical(bottom: Radius.circular(16)),
+          ),
+          child: ListTile(
+            title: Center(
+              child: Text('Search', style: TextStyle(color: BlaColors.white)),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
--- test/test_screen.dart
+++ test/test_screen.dart
@@ -1,5 +1,6 @@
-import 'package:blabla/ui/theme/theme.dart';
-import 'package:blabla/ui/widgets/actions/bla_button.dart';
+import 'package:blabla/ui/screens/ride_pref/widgets/ride_pref_form.dart';
+// import 'package:blabla/ui/theme/theme.dart';
+// import 'package:blabla/ui/widgets/actions/bla_button.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
@@ -8,15 +9,7 @@ void main(List<String> args) {
       home: Scaffold(
         backgroundColor: Colors.white,
         body: Center(
-          child: Container(
-            decoration: BoxDecoration(color: Colors.red),
-            child: BlaButton(
-              'data',
-              onPressed: () {},
-              buttonHierarchy: BlaButton.secondary,
-              icon: Icons.chair,
-            ),
-          ),
+          child: RidePrefForm(),
         ),
       ),
     ),
