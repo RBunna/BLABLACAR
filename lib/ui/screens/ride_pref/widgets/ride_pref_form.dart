1046c6acd34f3d1c6936333b3e96f34a87082040
RBunna <bunnasovethroath@gmail.com>
2026-02-18 11:04:53 +0700

    BLA-004 – Implement the Location Picker

--- lib/ui/screens/ride_pref/widgets/ride_pref_form.dart
+++ lib/ui/screens/ride_pref/widgets/ride_pref_form.dart
@@ -1,5 +1,6 @@
 import 'package:blabla/ui/theme/theme.dart';
 import 'package:blabla/ui/widgets/display/bla_divider.dart';
+import 'package:blabla/ui/widgets/inputs/bla_location_picker.dart';
 import 'package:blabla/utils/date_time_util.dart';
 import 'package:flutter/material.dart';
 
@@ -46,6 +47,22 @@ class _RidePrefFormState extends State<RidePrefForm> {
   // ----------------------------------
   // Handle events
   // ----------------------------------
+  Future<Location?> onLocationTileTap() async {
+    Location? pickedLocation = await Navigator.push(
+      context,
+      MaterialPageRoute<Location>(builder: (context) => BlaLocationPicker()),
+    );
+
+    return pickedLocation;
+  }
+
+  void onLocationSwapPressed() {
+    Location? temporaryLocation = departure;
+    setState(() {
+      departure = arrival;
+      arrival = temporaryLocation;
+    });
+  }
 
   // ----------------------------------
   // Compute the widgets rendering
@@ -65,6 +82,11 @@ class _RidePrefFormState extends State<RidePrefForm> {
       arrival != null ? _titleEnable(arrival!.name) : _titleLight('Going to');
   Text get date => _titleEnable(DateTimeUtils.formatDateTime(departureDate));
   Text get seat => _titleEnable(requestedSeats.toString());
+  bool get isSearchButtonEnable => (departure != null && arrival != null);
+  Color get searchButtonColor =>
+      isSearchButtonEnable ? BlaColors.primary : BlaColors.disabled;
+  Color get searchTextColor =>
+      isSearchButtonEnable ? BlaColors.white : BlaColors.neutralDark;
 
   // ----------------------------------
   // Build the widgets
@@ -81,25 +103,48 @@ class _RidePrefFormState extends State<RidePrefForm> {
               child: ListTile(
                 leading: Icon(Icons.circle_outlined),
                 title: departureTitle,
+                onTap: () async {
+                  Location? pickedLocation = await onLocationTileTap();
+                  if (pickedLocation != null && pickedLocation != departure) {
+                    setState(() {
+                      departure = pickedLocation;
+                    });
+                  }
+                },
               ),
             ),
-            if (departure != null || arrival != null) IconButton(onPressed: () {}, icon: Icon(Icons.swap_vert_outlined, color: BlaColors.primary,)),
+            if (departure != null || arrival != null)
+              IconButton(
+                onPressed: onLocationSwapPressed,
+                icon: Icon(Icons.swap_vert_outlined, color: BlaColors.primary),
+              ),
           ],
         ),
         BlaDivider(),
-        ListTile(leading: Icon(Icons.circle_outlined), title: arrivalTitle),
+        ListTile(
+          leading: Icon(Icons.circle_outlined),
+          title: arrivalTitle,
+          onTap: () async {
+            Location? pickedLocation = await onLocationTileTap();
+            if (pickedLocation != null && pickedLocation != arrival) {
+              setState(() {
+                arrival = pickedLocation;
+              });
+            }
+          },
+        ),
         BlaDivider(),
         ListTile(leading: Icon(Icons.calendar_month_outlined), title: date),
         BlaDivider(),
         ListTile(leading: Icon(Icons.person_outline), title: seat),
         Container(
           decoration: BoxDecoration(
-            color: BlaColors.primary,
+            color: searchButtonColor,
             borderRadius: BorderRadius.vertical(bottom: Radius.circular(16)),
           ),
           child: ListTile(
             title: Center(
-              child: Text('Search', style: TextStyle(color: BlaColors.white)),
+              child: Text('Search', style: TextStyle(color: searchTextColor)),
             ),
           ),
         ),
--- test/test_screen.dart
+++ test/test_screen.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 void main(List<String> args) {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.white,
         body: Center(
